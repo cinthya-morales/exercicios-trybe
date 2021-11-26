cb15250a4a502d05b3f2f3a2263a335136c33682
Cinthya Morales <cinthya.morales@outlook.com>
2021-11-26 19:23:57 -0300

    Exercicios javascript

---
+++ 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,23 @@
+//Exercícios 4.1
+const a = 10;
+const b = 5;
+//Adição (a + b)
+var resultado = a + b;
+console.log(resultado);
+//Subtração (a - b)
+var resultado = a - b;
+console.log(resultado);
+//Multiplicação (a * b)
+var resultado = a * b;
+console.log(resultado);
+//Divisão (a / b)
+var resultado = a / b;
+console.log(resultado);
+//Módulo (a % b)
+var resultado = a % b;
+console.log(resultado);
+
+const c = 10;
+const d = 20;
+If(c > d)
+console.log(c);
