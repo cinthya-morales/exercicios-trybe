743c4b681eb0f1bf5c97f49eaea60abd16f43cb0
Cinthya Morales <cinthya.morales@outlook.com>
2021-12-01 15:42:49 -0300

    organizando exercicio 4.3

--- 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios-4.3.js
+++ 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios-4.3.js
@@ -1,6 +1,6 @@
 //Encontrei os 2 exercicios aqui https://cursos.alura.com.br/forum/topico-escreva-um-codigo-que-imprima-um-quadrado-de-asteriscos-161926
 
-//Exercicios 1
+//Exercicio 1
 var n = 5;
 var asterisco = "";
 for (var linha = 1; linha <= n; linha++) {
@@ -10,14 +10,26 @@ for (var linha = 1; linha <= n; linha++) {
     console.log(asterisco);
     asterisco = "";
 }
-//Exercicios 2
+//Exercicio 2
 var n = 5;
-var asterisco = '';
+var asterisco = "";
 for (var linha = 1; linha <= n; linha++) {
     for (var coluna = 1; coluna <= n; coluna++) {
         if (coluna == linha) {
-            asterisco += '*'
+            asterisco += "*";
         }
     }
-    console.log(asterisco)
+    console.log(asterisco);
+}
+//Exercicio 3
+var n = 5;
+var asterisco = "*";
+var espaco = "";
+for (var linha = n; linha > 0; linha -= 1) {
+    for (var coluna = n; coluna > 0; coluna -= 1) {
+        if (coluna == linha) {
+            asterisco += "*";
+        }
+    }
+    console.log(asterisco);
 }
